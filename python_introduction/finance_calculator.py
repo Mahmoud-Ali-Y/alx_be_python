2fda29271f48df9f975be603d9e377b13bf8b48a
Mahmoud Ali <mahmoudaliy1492@gmail.com>
2024-09-14 23:27:41 +0300

    neverless alx sucks

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
-monthlyincome = int(input("Enter your monthly income: "))
-monthlyexpenses = int(input("Enter your total monthly expenses: "))
-MonthlySavings = monthlyincome - monthlyexpenses
-ProjectedSavings = int((MonthlySavings * 12) + (MonthlySavings * 12 * 0.05))
-print ("Your monthly savings are $", MonthlySavings, ".")
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
+monthly_Savings = monthly_income - monthly_expenses
+ProjectedSavings = int((monthly_Savings * 12) + (monthly_Savings * 12 * 0.05))
+print ("Your monthly savings are $", monthly_Savings, ".")
 print ("Projected savings after one year, with interest, is: $", ProjectedSavings, ".")
