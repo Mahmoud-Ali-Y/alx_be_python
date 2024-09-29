ccd2b40b66893ab7184a26bbfe3f2d0bccefb0a2
Mahmoud Ali <mahmoudaliy1492@gmail.com>
2024-09-29 23:52:12 +0300

    Second commit

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,36 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        print()
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            # Prompt for and add an item
+            new_item = input("Please enter the desired item to be added: ")
+            shopping_list.append(new_item)
+        elif choice == '2':
+            # Prompt for and remove an item
+            removed_item = input("Please enter the desired item to be removed: ")
+            shopping_list.remove(removed_item)
+            pass
+        elif choice == '3':
+            # Display the shopping list
+            for item in shopping_list:
+                print(item, end =" ")
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
