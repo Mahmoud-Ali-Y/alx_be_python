1efa455c891e521f667a44005a77ced163f2556d
Mahmoud Ali <mahmoudaliy1492@gmail.com>
2024-10-06 08:07:35 +0300

    First Commit

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,13 @@
+class BankAccount:
+    def __init__(self, amount):
+        self.account_balance = amount
+    def deposit(self, amount):
+        self.account_balance += amount
+    def withdraw(self, amount):
+        if self.account_balance >= amount:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance}")
