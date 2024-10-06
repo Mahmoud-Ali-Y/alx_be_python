7f989580e966462ef62323e4ee34c30aba1c4e47
Mahmoud Ali <mahmoudaliy1492@gmail.com>
2024-10-06 08:14:46 +0300

    First Commit

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,6 +1,7 @@
 class BankAccount:
     def __init__(self, amount):
-        self.account_balance = amount
+        self.account_balance = 0
+        self.account_balance += amount
     def deposit(self, amount):
         self.account_balance += amount
     def withdraw(self, amount):
