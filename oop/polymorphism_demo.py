bd13d71aa65049ef16dc3ec02781f71f9be0a661
Mahmoud Ali <mahmoudaliy1492@gmail.com>
2024-10-12 08:50:31 +0300

    Third Commit

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,15 @@
+import math
+class Shape:
+    def area(self):
+        raise NotImplementedError
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+    def area(self):
+        return self.length * self.width
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        return math.pi * pow(self.radius, 2)
