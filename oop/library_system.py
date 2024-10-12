269181b59146defd2ca426530181dfe908384352
Mahmoud Ali <mahmoudaliy1492@gmail.com>
2024-10-12 07:36:26 +0300

    Second Commit

--- oop/library_system.py
+++ oop/library_system.py
@@ -9,7 +9,7 @@ class EBook(Book):
         self.file_size = file_size
         super().__init__(title, author)
     def __str__(self):
-        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size} "
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         self.page_count = page_count
